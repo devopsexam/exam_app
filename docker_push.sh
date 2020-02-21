4976f8ee82090ccd8de98db105852aa5b46021ed
Margrethe <margrethethorsen@gmail.com>
2020-02-21 21:13:00 +0100

    Test

--- docker_push.sh
+++ docker_push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker build -t devops_application --build-arg JAR_FILE=target/eksamenoving-0.0.1-SNAPSHOT.jar .
+docker build -t devops_application --build-arg JAR_FILE=target/devops_application-0.0.1-SNAPSHOT.jar .
 docker tag devops_application "${DOCKER_USERNAME}"/devops_application
 docker push "${DOCKER_USERNAME}"/devops_application
